80f6861f27bfc7bd1718145e163afd0eefdeb7dc
alexandermeade <thisguys348@gmail.com>
2026-01-03 06:06:07 -0500

    Added type composition and tests

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,18 @@
 use jason_rs;
+use std::time::Instant;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let json = jason_rs::JasonBuilder::new()
+    let start = Instant::now();
+    let _json = jason_rs::JasonBuilder::new()
         .include_lua(r#"
             function inc(n) 
                 return n + 1
             end
         "#)?
-        .jason_to_json("./test.jason")?;
+        .jason_to_json("./testing.jason")?;
         //.jason_to_json("./test.jason")?;
-    println!("{}", json);
+    let duration = start.elapsed();
+    println!("some_function took: {:?}", duration);
+    println!("{}", _json);
     Ok(())
 }

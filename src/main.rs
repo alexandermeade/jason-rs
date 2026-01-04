29592c04a27c3e51c59a11c777e13ae6865a590e
alexandermeade <thisguys348@gmail.com>
2026-01-04 10:29:41 -0500

    added ` operator AKA variance operator

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,7 @@
-use jason_rs;
-use std::time::Instant;
+
+use serde_json;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let start = Instant::now();
     let _json = jason_rs::JasonBuilder::new()
         .include_lua(r#"
             function inc(n) 
@@ -11,8 +10,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         "#)?
         .jason_to_json("./testing.jason")?;
         //.jason_to_json("./test.jason")?;
-    let duration = start.elapsed();
-    println!("some_function took: {:?}", duration);
-    println!("{}", _json);
+
+    println!("{:#?}", _json);
     Ok(())
 }
